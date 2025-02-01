0a5f080011684491e1c5a611d35fa5f1102942a7
Ashel Vasquez <ashel.vasquez@unmsm.edu.pe>
2025-02-01 09:54:38 -0500

    Initial commit

---
+++ test/ep-infrastructure.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EpInfrastructure from '../lib/ep-infrastructure-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ep-infrastructure-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EpInfrastructure.EpInfrastructureStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
